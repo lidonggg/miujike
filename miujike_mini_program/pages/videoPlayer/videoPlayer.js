dcca7eedb36dcd77406e67d0b1b34cd9a50b224b
lidonggg <1225893478@qq.com>
2019-05-05 00:40:40 +0800

    视频播放页面

---
+++ miujike_mini_program/pages/videoPlayer/videoPlayer.js
@@ -0,0 +1,72 @@
+// pages/videoPlayer/videoPlayer.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    videoInfo:{
+      duration:"03:55",
+      playTimes:310,
+      singer:"聂家成",
+      title:"圣诞节",
+      originalSinger:"李东"
+    }
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
