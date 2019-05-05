d41b29c470d7b99e23ecf1609f78bc8dbd95aa7f
lidonggg <1225893478@qq.com>
2019-05-05 21:42:33 +0800

    推荐视频

--- miujike_mini_program/pages/videoPlayer/videoPlayer.js
+++ miujike_mini_program/pages/videoPlayer/videoPlayer.js
@@ -11,7 +11,48 @@ Page({
       singer:"聂家成",
       title:"圣诞节",
       originalSinger:"李东"
-    }
+    },
+    videoRecommended: [{
+      videoId: 1,
+      title: "圣诞节",
+      originalSinger: "陈奕迅",
+      singer: "聂家成",
+      listenTimes: 100,
+      duration: "03:00",
+      cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+    }, {
+      videoId: 2,
+      title: "圣诞节",
+      originalSinger: "陈奕迅",
+      singer: "聂家成",
+      listenTimes: 100,
+      duration: "03:00",
+      cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+    }, {
+      videoId: 3,
+      title: "圣诞节",
+      originalSinger: "陈奕迅",
+      singer: "聂家成",
+      listenTimes: 100,
+      duration: "03:00",
+      cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+    }, {
+      videoId: 4,
+      title: "圣诞节",
+      originalSinger: "陈奕迅",
+      singer: "聂家成",
+      listenTimes: 100,
+      duration: "03:00",
+      cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+    }, {
+      videoId: 5,
+      title: "圣诞节",
+      originalSinger: "陈奕迅",
+      singer: "聂家成",
+      listenTimes: 100,
+      duration: "03:00",
+      cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+    }]
   },
 
   /**
