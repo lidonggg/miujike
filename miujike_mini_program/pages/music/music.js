70d5aa11e4e30cb47fbd080fb8d336cd1f7e04be
lidonggg <1225893478@qq.com>
2019-05-11 16:31:05 +0800

    musci主页面

--- miujike_mini_program/pages/music/music.js
+++ miujike_mini_program/pages/music/music.js
@@ -5,76 +5,172 @@ Page({
    * Page initial data
    */
   data: {
-    searchValue:""
+    searchValue: "",
+    newMusicList: [{
+        musicId: 1,
+        title: "圣诞节,,,........",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 1000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      },
+      {
+        musicId: 2,
+        title: "圣诞节,,,,,",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 1000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      },
+      {
+        musicId: 3,
+        title: "圣诞节",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 1000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      }
+    ],
+    musicList: [{
+        musicId: 1,
+        title: "圣诞节,,,........",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 1000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      },
+      {
+        musicId: 2,
+        title: "圣诞节,,,,,",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 1000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      },
+      {
+        musicId: 3,
+        title: "圣诞节",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 10000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      }
+      ,
+      {
+        musicId: 4,
+        title: "圣诞节",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 10000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      }
+      ,
+      {
+        musicId: 5,
+        title: "圣诞节",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 10000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      },
+      {
+        musicId: 6,
+        title: "圣诞节",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 10000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      },
+      {
+        musicId: 7,
+        title: "圣诞节",
+        singer: "ld",
+        originalSinger: "dongdong",
+        duration: "03:00",
+        playTimes: 10000,
+        cover: "http://pq3gqpelo.bkt.clouddn.com/2019-05-04-46707c55-1105-420a-b8f4-dd908bb09b67.png"
+      }
+    ]
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
-
+  onPullDownRefresh: function() {
+    setTimeout(function() {
+      wx.stopPullDownRefresh();
+    }, 2000)
   },
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
   /**
    * 输入
    */
-  doInput(e){
+  doInput(e) {
     this.setData({
-      searchValue:e.detail.value
+      searchValue: e.detail.value
     })
   },
   /**
    * 搜索
    */
-  doSearch(e){
+  doSearch(e) {
     let that = this;
     wx.showToast({
       title: that.data.searchValue
